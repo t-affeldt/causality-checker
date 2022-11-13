23c4f6da29db815f27c7662e136661207459f203
Till Affeldt <t.affeldt@tu-braunschweig.de>
2022-11-13 21:46:02 +0100

    Implement k-means clustering

--- index.py
+++ index.py
@@ -1,32 +1,49 @@
+import os
+import shutil
+
 import pandas as pd
 import kmeans1d
 
-print('Reading table')
-df = pd.read_csv('dummy_data.csv')
+# ordered by increasing values of clusters
+# number of cluster names determines cluster count
+CLUSTER_NAMES = [ 'lower', 'upper' ]
 
-meta = pd.DataFrame({
-    'lower': [],
-    'upper': []
-})
+def clean_folder():
+    for root, dirs, files in os.walk('./indices'):
+        for f in files:
+            os.unlink(os.path.join(root, f))
+        for d in dirs:
+            shutil.rmtree(os.path.join(root, d))
 
-for col in df.columns:
-    print('processing column ' + col)
-    # skip if not numeric
-    if (not df.dtypes[col] in ['int64', 'float64']):
-        continue
-    # split on median
-    divider = df[col].median()
+def generate_indices():
+    cluster_count = len(CLUSTER_NAMES)
+    print('Reading table')
+    df = pd.read_csv('dummy_data.csv', index_col = False)
 
-    lower = df[col][df[col] < divider]
-    lower.to_csv(path_or_buf = "./indices/" + col + "_lower.csv", header = False)
-    lower_count = lower.shape[0]
+    meta = pd.DataFrame(dict(zip(CLUSTER_NAMES, [[]] * cluster_count)))
+    print(meta)
 
-    upper = df[col][df[col] >= divider]
-    upper.to_csv(path_or_buf = "./indices/" + col + "_upper.csv", header = False)
-    upper_count = upper.shape[0]
+    for col in df.columns:
+        print('processing column ' + col)
+        # skip if not numeric
+        if (not df.dtypes[col] in ['int64', 'float64']):
+            continue
+        data = df[col].to_numpy()
+        clusters, centroids = kmeans1d.cluster(data, cluster_count)
 
-    meta.loc[col] = [ lower_count, upper_count ]
+        # store index in text files
+        s = pd.Series(clusters)
+        counter = [0] * cluster_count
+        for i in range(0, cluster_count):
+            selection = s[s == i]
+            counter[i] = selection.size
+            selection.to_csv('indices/' + col + '_' + CLUSTER_NAMES[i] +'.txt', columns = [], header = False)
+        meta.loc[col] = counter
 
-meta.to_csv(path_or_buf = "./indices/_meta.csv", index_label = 'column')
+    meta.to_csv('indices/_meta.csv', index_label = 'column')
 
-print('Finished processing')
+    print('Finished processing')
+
+
+clean_folder()
+generate_indices()
--- metrics.py
+++ metrics.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
 ARG1 = "normaldist"
-ARG2 = "normaldist1"
-
-meta = pd.read_csv('indices/_meta.csv', index_col = 'column')
+ARG2 = "normaldist"
 
 def correlation(col1, col2):
     print('reading table')
@@ -13,30 +11,25 @@ def correlation(col1, col2):
     print(corr)
 
 def aLift(cause, c_index, effect, e_index):
-    total = meta.loc[effect].lower + meta.loc[effect].upper
-    current = 0
-    matches = 0
-    matches_pos = 0
-
     # Read indices
     print('reading indices')
-    file1 = open("indices/" + cause + "_" + c_index + ".csv", 'r')
-    file2 = open("indices/" + effect + "_" + e_index + ".csv", 'r')
+    meta = pd.read_csv('indices/_meta.csv', index_col = 'column')
+    file1 = open("indices/" + cause + "_" + c_index + ".txt", 'r')
+    file2 = open("indices/" + effect + "_" + e_index + ".txt", 'r')
 
     # Find intersection of search spaces (linearly)
     print('finding intersection space')
+    matches = 0
+    matches_pos = 0
     line1 = file1.readline()
     line2 = file2.readline()
-
     while line1 and line2:
-        id1 = line1.strip().split(',')[0]
-        id2 = line2.strip().split(',')[0]
-        if (id1 == id2):
+        if (line1 == line2):
             line1 = file1.readline()
             line2 = file2.readline()
             matches += 1
             matches_pos += 1
-        elif (id1 < id2):
+        elif (line1 < line2):
             line1 = file1.readline()
             matches += 1
         else:
@@ -46,6 +39,7 @@ def aLift(cause, c_index, effect, e_index):
     file2.close()
 
     print('calculating aLift')
+    total = meta.loc[effect].lower + meta.loc[effect].upper
     prob_total = meta.loc[effect][e_index] / total
     prob_match = matches_pos / matches
     aLift = prob_match - prob_total
